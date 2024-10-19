ed47e38e94884dee870adff2dbbc508da9822d55
hazecodez <muhammedhashir298@gmail.com>
2024-10-19 23:39:37 +0530

    set access token as the bearer

--- src/Services/Apis.jsx
+++ src/Services/Apis.jsx
@@ -4,6 +4,21 @@ const axiosInstance = axios.create({
   baseURL: "http://82.180.145.66:8000/api/v1",
 });
 
+axiosInstance.interceptors.request.use(
+  (config) => {
+    if (config && config.url) {
+      const token = localStorage.getItem("token");
+      if (token) {
+        config.headers["Authorization"] = `Bearer ${token}`;
+      }
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export async function userLogin(formdata) {
   try {
     const response = await axiosInstance.post("/login", formdata);
