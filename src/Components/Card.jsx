34cfe50c80580effd5f6109c3086effa6b6305a1
hazecodez <muhammedhashir298@gmail.com>
2024-10-22 22:06:44 +0530

    Final Commit

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -32,36 +32,38 @@ export default function Card({
     <>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 lg:pl-8 1290px:grid-cols-5">
         {tasks.map((task, index) => (
-          <>
-            <div
-              key={index}
-              className={`card ${
-                task.status === true
-                  ? "bg-[rgb(2,48,32)]"
-                  : "bg-[rgb(139,128,0)]"
-              }`}
-            >
-              <div className="text" onClick={() => detailsModal(task)}>
-                <span>{task.title}</span>
-                <p className="subtitle text-white line-clamp-3">{task.description}</p>
-              </div>
-              <div className="icons flex w-full justify-evenly">
-                <a className="btn w-full" onClick={() => editFormModal(task)}>
-                  <RiEditFill className="hover:text-gray-800 text-2xl" />
-                </a>
-                <a className="btn w-full" onClick={() => updateTask(task, task.id)}>
-                  <input
-                    type="checkbox"
-                    checked={task.status}
-                    className="appearance-none w-5 h-5 bg-gray-200 border-2 border-gray-400 rounded-md checked:bg-green-500 checked:border-green-800"
-                  />
-                </a>
-                <a className="btn w-full" onClick={() => deleteModal(task.id)}>
-                  <MdDelete className="hover:text-gray-800 text-2xl" />
-                </a>
-              </div>
+          <div
+            key={index}
+            className={`card ${
+              task.status === true ? "bg-[rgb(2,48,32)]" : "bg-[rgb(139,128,0)]"
+            }`}
+          >
+            <div className="text" onClick={() => detailsModal(task)}>
+              <span>{task.title}</span>
+              <p className="subtitle text-white line-clamp-3">
+                {task.description}
+              </p>
             </div>
-          </>
+            <div className="icons flex w-full justify-evenly">
+              <a className="btn w-full" onClick={() => editFormModal(task)}>
+                <RiEditFill className="hover:text-gray-800 text-2xl" />
+              </a>
+              <a
+                className="btn w-full"
+                onClick={() => updateTask(task, task.id)}
+              >
+                <input
+                  type="checkbox"
+                  checked={task.status}
+                  onChange={() => updateTask(task, task.id)}
+                  className="appearance-none w-5 h-5 bg-gray-200 border-2 border-gray-400 rounded-md checked:bg-green-500 checked:border-green-800"
+                />
+              </a>
+              <a className="btn w-full" onClick={() => deleteModal(task.id)}>
+                <MdDelete className="hover:text-gray-800 text-2xl" />
+              </a>
+            </div>
+          </div>
         ))}
       </div>
     </>
