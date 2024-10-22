d0c2257314713222fc095fed6ec4273c7ebdb473
hazecodez <muhammedhashir298@gmail.com>
2024-10-22 21:59:12 +0530

    backend url stored in env

--- src/Services/Apis.jsx
+++ src/Services/Apis.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://82.180.145.66/api/v1",
+  baseURL: import.meta.env.VITE_BACKEND_URL,
 });
 
 axiosInstance.interceptors.request.use(
